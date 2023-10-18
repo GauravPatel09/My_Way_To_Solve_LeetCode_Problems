a2c779262ae89994dd578f9bef2978a9a3cc7cdb
GauravPate09 <gauravjpatel09@gmail.com>
2023-10-18 18:07:24 -0400

    first commit

--- License_Key_Formatting.java
+++ License_Key_Formatting.java
@@ -1,6 +1,16 @@
-package org.example.leetcode75.My_Way_To_Solve_LeetCode_Problems;
+package org.bank.demo.LeetCodeOnly;
 
 public class License_Key_Formatting {
+    //    482. License Key Formatting      (Easy)
+//    You are given a license key represented as a string s that consists of only alphanumeric characters and dashes. The string is separated into n + 1 groups by n dashes. You are also given an integer k.
+//    We want to reformat the string s such that each group contains exactly k characters, except for the first group, which could be shorter than k but still must contain at least one character. Furthermore, there must be a dash inserted between two groups,
+//    ,and you should convert all lowercase letters to uppercase.
+//    Return the reformatted license key.
+//      Example 1:
+//    Input: s = "5F3Z-2e-9-w", k = 4
+//    Output: "5F3Z-2E9W"
+//    Explanation: The string s has been split into two parts, each part has 4 characters.
+//    Note that the two extra dashes are not needed and can be removed.
     public static String  licenseKeyFormatting(String s, int k) {
         String input = s.replaceAll("-", "").toUpperCase();
         int lengthOfString = input.length();
@@ -10,6 +20,7 @@ public class License_Key_Formatting {
             if (counter == 0) {
                 output = output.append("-");
                 counter = k;
+                System.out.println("");
             }
             counter--;
             output = output.append(String.valueOf(input.charAt(i)));
