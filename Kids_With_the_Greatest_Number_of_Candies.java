e6e29594a4523c4dd19d74feb1bee1ee22e15bac
GauravPate09 <gauravjpatel09@gmail.com>
2023-10-19 13:40:08 -0400

    Kids_with_greatest_number_of candies is solved with java code

--- Kids_With_the_Greatest_Number_of_Candies.java
+++ Kids_With_the_Greatest_Number_of_Candies.java
@@ -1,9 +1,9 @@
-package org.example.leetcode75.My_Way_To_Solve_LeetCode_Problems;
+package org.example.My_Way_To_Solve_LeetCode_Problems;
 
+import java.util.ArrayList;
 import java.util.List;
-
 public class Kids_With_the_Greatest_Number_of_Candies {
-//    1431. Kids With the Greatest Number of Candies Easy
+    //    1431. Kids With the Greatest Number of Candies (Easy)
 //    There are n kids with candies. You are given an integer array candies, where each candies[i] represents the number of candies the ith kid has, and an integer extraCandies, denoting the number of extra candies that you have.
 //    Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies, they will have the greatest number of candies among all the kids, or false otherwise.
 //    Note that multiple kids can have the greatest number of candies.
@@ -16,12 +16,32 @@ public class Kids_With_the_Greatest_Number_of_Candies {
 //              - Kid 3, they will have 5 + 3 = 8 candies, which is the greatest among the kids.
 //              - Kid 4, they will have 1 + 3 = 4 candies, which is not the greatest among the kids.
 //              - Kid 5, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
-
-
-    class Solution {
-        public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
-
+    static class Solution {
+        public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+            List<Boolean> result = new ArrayList<>();
+            int max_candy = 0;
+            for (int candy : candies) {
+                max_candy = Math.max(max_candy, candy);
+            }
+            for (int candy : candies) {
+                result.add(max_candy <= extraCandies + candy);
+//               this line same works as bellow Code
+//                if (max_candy <= extraCandies + candy) {
+//                    result.add(true);
+//                } else {
+//                    result.add(false);
+//                }
+            }
+            return result;
+            // candies_list.stream().filter(extraCandies->extraCandies+integer).forEach(Boolean flag -> true);=
         }
+    }
+
+    public static void main(String[] args) {
+        int[] candies = {2, 3, 5, 1, 3};
+        List<Boolean> result = Solution.kidsWithCandies(candies, 3);
+        System.out.println(result);
+
     }
 
 }
