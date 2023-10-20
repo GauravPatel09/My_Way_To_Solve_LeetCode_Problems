09de9ed8c11189954d2fe606bb730b015cb862c2
GauravPate09 <gauravjpatel09@gmail.com>
2023-10-19 22:45:02 -0400

    CanPlaceFlower Solved with comments

--- Can_place_flowers.java
+++ Can_place_flowers.java
@@ -1,6 +1,14 @@
 package org.example.My_Way_To_Solve_LeetCode_Problems;
 
 public class Can_place_flowers {
+//    You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+//    Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+//    Example 1:
+//    Input: flowerbed = [1,0,0,0,1], n = 1
+//    Output: true
+//    Example 2:
+//    Input: flowerbed = [1,0,0,0,1], n = 2
+//    Output: false
 
         public static boolean canPlaceFlowers(int[] flowerbed, int n) {
 
